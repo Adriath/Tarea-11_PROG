76b2bc83a8ce77dce806a714fed07d7dafe4262f
Adriath <adrianarjonabravo@gmail.com>
2023-06-02 20:15:31 +0200

    Boceto de la clase Partida terminado.

--- src/aplicacion/modelos/Partida.java
+++ src/aplicacion/modelos/Partida.java
@@ -1,6 +1,7 @@
 
 package aplicacion.modelos;
 
+import aplicacion.modelos.excepciones.ExcepcionPartida;
 import java.util.ArrayList;
 
 /**
@@ -17,10 +18,27 @@ public class Partida {
     
     // -------------- DECLARACIÓN DE VARIABLES ---------------------
     
+    /**
+     * Número de rondas que tendrá la partida.
+     * De 1 a 5 rondas.
+     */
     private byte rondas ;
+    
+    /**
+     * Número de jugadores de la partida.
+     * De 2 a 6 jugadores.
+     */
     private byte numJugadores ;
+    
+    /**
+     * Número de puntos del jugador.
+     */
     private int puntos ;
-    private ArrayList<Jugador> listaJugadores ;
+    
+    /**
+     * Lista de jugadores de la partida.
+     */
+    private Jugador[] listaJugadores ;
     
     
     // ------------------ DECLARACIÓN DE MÉTODOS ------------------------
@@ -34,8 +52,7 @@ public class Partida {
         this.rondas = 0 ;
         this.numJugadores = 0 ;
         this.puntos = 0 ;
-        this.listaJugadores = null ;
-            
+        this.listaJugadores = new Jugador[6] ;
         }
 
     
@@ -49,46 +66,130 @@ public class Partida {
         
         // --- GETTERS & SETTERS ---
 
+        
             // RONDAS
         
+            /**
+             * Método que devuelve el número de rondas.
+             * 
+             * @return Devuelve el número de rondas.
+             */
             public byte getRondas() {
+                
                 return rondas;
             }
 
-            public void setRondas(byte rondas) {
-                this.rondas = rondas;
+            
+            /**
+             * Método que modifica el número de rondas.
+             * 
+             * @param rondas Número de rondas de la partida.
+             * @throws ExcepcionPartida 
+             */
+            public void setRondas(byte rondas) throws ExcepcionPartida{
+                
+                if ( (rondas > 0) & (rondas <= 5)) 
+                {
+                    this.rondas = rondas ;
+                }
+                else
+                {
+                    throw new ExcepcionPartida("Puedes jugar desde 1 hasta 5 rondas.") ;
+                }
             }
 
+            
+            
             // NÚMERO DE JUGADORES
             
+            
+            /**
+             * Método que devuelve el número de jugaodores.
+             * 
+             * @return Devuelve el número de jugadores.
+             */
             public byte getNumJugadores() {
+                
                 return numJugadores;
             }
 
-            public void setNumJugadores(byte numJugadores) {
-                this.numJugadores = numJugadores;
+            
+            
+            /**
+             * Método que modifica el número de jugadores de una partida.
+             * 
+             * @param numJugadores Número de jugadores.
+             * @throws ExcepcionPartida 
+             */
+            public void setNumJugadores(byte numJugadores) throws ExcepcionPartida{
+                
+                if ( (numJugadores >= 2) && (numJugadores <= 6 )) 
+                {
+                    this.numJugadores = numJugadores ;
+                }
+                else
+                {
+                    throw new ExcepcionPartida("La partida es entre 2-6 jugadores.") ;
+                }
             }
 
+            
+            
             // PUNTOS
             
+            /**
+             * Método que devuelve el número de puntos en la partida.
+             * 
+             * @return Devuelve el número de puntos.
+             */
             public int getPuntos() {
+                
                 return puntos;
             }
 
+            
+            /**
+             * Método que modifica el número de puntos.
+             * 
+             * @param puntos Número de puntos.
+             */
             public void setPuntos(int puntos) {
+                
                 this.puntos = puntos;
             }
 
+            
+            
             // LISTA DE JUGADORES
             
-            public ArrayList<Jugador> getListaJugadores() {
+
+            /**
+             * Método que devuelve la lista de jugadores de la partida.
+             * 
+             * @return Devuelve la lista de jugadores
+             */
+            public Jugador[] getListaJugadores() {
+                
                 return listaJugadores;
             }
-
-            public void setListaJugadores(ArrayList<Jugador> listaJugadores) {
-                this.listaJugadores = listaJugadores;
+            
+            
+            
+            /**
+             * Método que modifica la lista de jugadores de la partida.
+             * 
+             * @param listaJugadores Lista de jugadores.
+             */
+            public void setListaJugadores(Jugador[] listaJugadores) throws ExcepcionPartida{
+                
+                if ( (numJugadores >= 2) && (numJugadores <= 6) )
+                {
+                    this.listaJugadores = listaJugadores;
+                }
+                else
+                {
+                    throw new ExcepcionPartida("El número de jugadores tiene que ser de entre 2 y 6.") ;
+                }
             }
-        
-        
     
 }
